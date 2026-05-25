18c37751e17c01a75725a5193234fe3f3fb959dd
alejandro <alejuggling1307@gmail.com>
2026-05-25 16:38:17 -0500

    clase 28 pantalla de candidatos

---
+++ app.js
@@ -0,0 +1,17 @@
+const botonesPerfil = document.querySelectorAll(".btnPerfil");
+const detallePerfil = document.getElementById("detallePerfil");
+const btnmensaje = document.getElementById("btnmensaje");
+const mensejeclase = document.getElementById("mensejeclase");
+
+let contadordecliks = 0;
+
+btnmensaje.addEventListener("click", () => {
+    mensejeclase.textContent = el texto que quisimos mostrar + contadordecliks
+    contadordecliks = contadordecliks + 1;
+} 
+
+botonesPerfil.forEach(function (boton) {
+  boton.addEventListener("click", function () {
+    const perfil = boton.getAttribute("data-perfil");
+    detallePerfil.textContent = `informacion detallada del perfil ${perfil}`;
+  });
